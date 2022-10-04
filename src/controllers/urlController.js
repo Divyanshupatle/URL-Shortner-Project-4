78e109d62134e273689cde7c138a27704562564d
John Doe <divyanshupatle123@gmail.com>
2022-10-04 09:51:23 +0530

    add redis

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,12 +2,34 @@ const UrlModel = require("../models/urlModel")
 const shortid = require('shortid')
 let validUrl = require('valid-url');
 const baseUrl = 'http://localhost:3000'
+const redis = require("redis");
+const { promisify } = require("util");
+
+//Connect to redis
+
+const redisClient = redis.createClient(
+    14020,
+    "redis-14020.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+    );
+    redisClient.auth("pVbprLOd85SalbxsCiRqriIaCDBamEmo", function (err) {
+    if (err) throw err;
+    });
+    
+    redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+    });
+    
+    //Connection setup for redis
+    
+    const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+    const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 const isValidAdd = function (value) {
     if (typeof value == "undefined" || value === null || typeof value === "boolean" || typeof value === "number") return false
     if (typeof value == "string" && value.trim().length == 0) return false
     return true
 }
-
 
 const creatUrl = async function (req, res) {
     try {
@@ -45,22 +67,20 @@ const creatUrl = async function (req, res) {
 
 const getUrl = async function (req, res) {
     try {
-        let data = req.param
-        let urlCode = data.urlCode
 
-        if (!isValidAdd(data)) {
-            return res.status(400).send({ status: false, message: "please provide data in params" })
-        }
-        if (!data) {
-            return res.status(400).send({ status: false, message: "urlCode is mandatory" })
-        }
+        let casheData = await GET_ASYNC(`${req.params.urlCode}`);
+
+        if (casheData) return res.status(302).redirect(casheData);
     
-        const url = await UrlModel.findOne({urlCode: req.params.urlCode})
-        if (url) {
-            return res.status(302).redirect(url.longUrl)
-        }
+        const findURL = await UrlModel.findOne({ urlCode: req.params.urlCode });
 
-        return res.status(404).send({status: false, message: "invalidUrl"})
+        if(findURL){
+        await SET_ASYNC(`${req.params.urlCode}`, findURL.longUrl);
+        return res.status(302).redirect(findURL.longUrl);
+        }
+        
+        return res.status(400).send({ status: false, message: "invalidUrl" })
+
     }
     catch (err) {
         res.status(500).send({ status: false, message: err })

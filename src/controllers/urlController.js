bfccf90a45774bef10254e99a2de0059f40d1146
John Doe <divyanshupatle123@gmail.com>
2022-10-07 10:04:18 +0530

    done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -11,19 +11,19 @@ const redisClient = redis.createClient(
     14020,
     "redis-14020.c212.ap-south-1-1.ec2.cloud.redislabs.com",
     { no_ready_check: true }
-    );
-    redisClient.auth("pVbprLOd85SalbxsCiRqriIaCDBamEmo", function (err) {
+);
+redisClient.auth("pVbprLOd85SalbxsCiRqriIaCDBamEmo", function (err) {
     if (err) throw err;
-    });
-    
-    redisClient.on("connect", async function () {
+});
+
+redisClient.on("connect", async function () {
     console.log("Connected to Redis..");
-    });
-    
-    //Connection setup for redis
-    
-    const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
-    const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+});
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const isValidAdd = function (value) {
     if (typeof value == "undefined" || value === null || typeof value === "boolean" || typeof value === "number") return false
@@ -36,7 +36,7 @@ const creatUrl = async function (req, res) {
         let data = req.body
         let longUrl = data.url
 
-        if (!isValidAdd(data)) {
+        if (Object.keys(data).length == 0) {
             return res.status(400).send({ status: false, message: "please provide data in body" })
         }
         if (!longUrl) {
@@ -48,7 +48,7 @@ const creatUrl = async function (req, res) {
         }
         const urlCode = shortid.generate().toLocaleLowerCase()
 
-        let url = await UrlModel.findOne({ longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+        let url = await UrlModel.findOne({ longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
         if (url) {
             return res.status(200).send({ status: true, data: url })
         }
@@ -57,7 +57,7 @@ const creatUrl = async function (req, res) {
 
         url = new UrlModel({ longUrl, shortUrl, urlCode })
         await url.save()
-        let obj = await UrlModel.findOne({ longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+        let obj = await UrlModel.findOne({ longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
         res.status(200).send({ status: true, data: obj })
 
     } catch (err) {
@@ -71,14 +71,14 @@ const getUrl = async function (req, res) {
         let casheData = await GET_ASYNC(`${req.params.urlCode}`);
 
         if (casheData) return res.status(302).redirect(casheData);
-    
+
         const findURL = await UrlModel.findOne({ urlCode: req.params.urlCode });
 
-        if(findURL){
-        await SET_ASYNC(`${req.params.urlCode}`, findURL.longUrl);
-        return res.status(302).redirect(findURL.longUrl);
+        if (findURL) {
+            await SET_ASYNC(`${req.params.urlCode}`, findURL.longUrl);
+            return res.status(302).redirect(findURL.longUrl);
         }
-        
+
         return res.status(404).send({ status: false, message: "url not found" })
 
     }
